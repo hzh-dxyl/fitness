e787f68b9b4957fd96ebd8be9a4d0e80d45ad4fe
hzh-dxyl <13544968369@163.com>
2022-01-14 21:46:05 +0800

    增加全局量

--- src/main/java/com/hzh/fitness/common/GlobalConstant.java
+++ src/main/java/com/hzh/fitness/common/GlobalConstant.java
@@ -11,12 +11,15 @@ public class GlobalConstant {
 
     public static String PYTHON_PORT;
     public static String PYTHON_PATH;
-
     public static String PROJECT_ROOT;
-
     public static String IMAGE_ROOT;
-    public static final String[] DEFAULT_HEAD_HEX={
-
+    public static String PROJECT_HOST;
+    public static String PYTHON_HOST;
+    public static final String[] DEFAULT_HEAD_HEX = {
+            "7f714737bbcded399458436308f508f5bb77201b",
+            "e0da8634f28b156faa986d6651a392dd4a915979",
+            "34a4a70f005bb646ed20bf94f621407f98c9c3d5",
+            "15ff0209e28791a8f85d09ac4a6d1f5ae9acf07a"
     };
 
     @Value("${projectRoot}")
@@ -37,5 +40,15 @@ public class GlobalConstant {
     @Value("${pythonPort}")
     public void setPythonPort(String pythonPort) {
         PYTHON_PORT = pythonPort;
+    }
+
+    @Value("${projectHost}")
+    public static void setProjectHost(String projectHost) {
+        PROJECT_HOST = projectHost;
+    }
+
+    @Value("${pythonHost}")
+    public static void setPythonHost(String pythonHost) {
+        PYTHON_HOST = pythonHost;
     }
 }
