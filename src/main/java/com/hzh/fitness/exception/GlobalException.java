5ca9a7bca055e6f602e548dd60a08925f7cdcb45
hzh-dxyl <13544968369@163.com>
2022-01-14 23:37:55 +0800

    增加全局异常捕获

--- src/main/java/com/hzh/fitness/exception/GlobalException.java
+++ src/main/java/com/hzh/fitness/exception/GlobalException.java
@@ -3,8 +3,21 @@ package com.hzh.fitness.exception;
 /**
  * @author hzh
  */
-public class GlobalException extends Exception {
+public class GlobalException extends RuntimeException {
+
+    private String msg;
+
     public GlobalException(String msg) {
         super(msg);
+        this.msg = msg;
+    }
+
+    @Override
+    public String getMessage() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
     }
 }
--- src/main/java/com/hzh/fitness/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hzh/fitness/exception/GlobalExceptionHandler.java
@@ -1,10 +1,30 @@
 package com.hzh.fitness.exception;
 
+import com.alibaba.fastjson.JSONObject;
+import com.hzh.fitness.common.MyResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.io.FileNotFoundException;
 
 /**
  * @author hzh
  */
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public MyResponse<JSONObject> commonExceptionHandler(Exception e) {
+        return MyResponse.createResponseByError("server error");
+    }
+
+    @ExceptionHandler(FileNotFoundException.class)
+    public MyResponse<JSONObject> fileNotFoundExceptionHandler() {
+        return MyResponse.createResponseByError("file not found");
+    }
+
+    @ExceptionHandler(GlobalException.class)
+    public MyResponse<JSONObject> globalExceptionHandler(GlobalException e){
+        return MyResponse.createResponseByError(e.getMessage());
+    }
+
 }
